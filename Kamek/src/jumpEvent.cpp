9b4ecbb2e2733112e6f76497819bc43822f00758
Jaydon <54336596+MakerMan-creator@users.noreply.github.com>
2022-07-01 21:06:32 -0400

    Multiple Events available (I think)

--- Kamek/src/jumpEvent.cpp
+++ Kamek/src/jumpEvent.cpp
@@ -28,7 +28,11 @@ daEnJumpEvent_c* daEnJumpEvent_c::build() {
 
 
 int daEnJumpEvent_c::onCreate() {
-    this->event = (this->settings >> 28 & 0xF) - 1;
+    this->event = (settings >> 24 & 0xFF) - 1; // Nybbles 5 and 6
+
+	if (this->event > 64) {
+		this->event = 64;
+	}
 
 	OSReport("Spawned!\n");
 
